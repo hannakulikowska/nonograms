3dce72ffd907a8bcb47d7e4ec0c189b11f3b71a9
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-02-05 16:14:43 +0100

    refactor: enhance readability and reusability of time calculation and results handling

--- nonograms/src/js/results.module.js
+++ nonograms/src/js/results.module.js
@@ -1,17 +1,11 @@
 import { minutesSpan, secondsSpan } from "./stopWatch.module.js";
 
 export function resultsData() {
+  const gameTimer = gameTime();
+  const totalInSeconds = totalSeconds(gameTimer);
+
   // Retrieve puzzle data from Local Storage
   const currentPuzzle = JSON.parse(localStorage.getItem("currentPuzzle"));
-
-  // Get time
-  let gameTime = {
-    minutes: minutesSpan.innerHTML,
-    seconds: secondsSpan.innerHTML,
-  };
-
-  // Total time in seconds
-  let totalInSeconds = Number(gameTime.minutes * 60) + Number(gameTime.seconds);
 
   // Get name and size of the current puzzle
   let puzzleName = currentPuzzle ? currentPuzzle.name : "Unknown";
@@ -20,7 +14,29 @@ export function resultsData() {
   // Retrieve existing results from Local Storage
   let existingResults = JSON.parse(localStorage.getItem("results")) || [];
   // Add the new game result
-  existingResults.push({ gameTime, totalInSeconds, puzzleName, puzzleSize });
+  existingResults.push({
+    gameTime: gameTimer,
+    totalInSeconds,
+    puzzleName,
+    puzzleSize,
+  });
   // Save updated results to Local Storage
   localStorage.setItem("results", JSON.stringify(existingResults));
+}
+
+export function gameTime() {
+  // Get time
+  let gameTime = {
+    minutes: minutesSpan.innerHTML,
+    seconds: secondsSpan.innerHTML,
+  };
+  return gameTime;
+}
+
+function totalSeconds() {
+  // Get gameTime.minutes and getTime.seconds
+  let { minutes, seconds } = gameTime();
+  // Total time in seconds
+  let totalInSeconds = Number(minutes * 60) + Number(seconds);
+  return totalInSeconds;
 }
